5ff7a5f18ef6b1fbc5fd0604ded68fbf312893f1
sk7930 <sk7930@nyu.edu>
2021-03-01 22:14:04 -0500

    hello.c

--- hello.c
+++ hello.c
@@ -2,5 +2,7 @@
  
 int main() {
    printf("Hello, world  test 2  !\n");
+   printf("\nThis is my first program after long time.\n");
+   printf("\nIt is like a found old girlfreind after 23 years later in FB.\n");
    return 0;
 }
